771ab4423f435584663af30adcb5b2b44a8f2ee6
Elias Marin <eliasmarinch@gmail.com>
2021-02-03 02:47:00 -0700

    F

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+
+const client = new Discord.Client();
+
+
+const prefix = process.env.PREFIX;
+
+client.on("message", (message) => {
+    if (message.author.bot) return;
+
+    const command = message.content;
+
+    if (command === "f" || command === "F") {
+        message.channel.send(`${message.member.user.username} madre mia willy`);
+    }
+});
+
+
+client.login(process.env.BOT_TOKEN);
