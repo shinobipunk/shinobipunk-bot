b544ea5ec06e28b167b21bcc4bf7365d71164766
Elias Marin <eliasmarinch@gmail.com>
2021-02-04 00:51:44 -0700

    Bto bot

--- index.js
+++ index.js
@@ -14,5 +14,34 @@ client.on("message", (message) => {
     }
 });
 
+client.on("message", (message) => {
+    if (message.author.bot) return;
+
+    const btoTexto = [
+        "bto",
+        "beto",
+        "Beto"
+    ];
+
+    const btoFrases = [
+        "Compra Doge",
+        "Te vendo unos audifonos gamer",
+        "Alguien dijo nepe",
+        "Ya Ferrales",
+        "Monstruo"
+    ];
+
+    const btoRespuesta = Math.floor(Math.random() * btoFrases.length);
+
+    const command = message.content;
+
+    for (let i = 0; i < btoTexto.length; i++) {
+        if (command.includes(btoTexto[i])) {
+            message.channel.send(btoFrases[btoRespuesta]);
+            break;
+        }
+    }
+});
+
 
 client.login(process.env.BOT_TOKEN);
